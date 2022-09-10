67d4a8c333adafbd46f8db9271f27c4576bc01b0
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-09-10 19:04:53 +0300

    resolve swipe task

--- 1-swipe/src/index.ts
+++ 1-swipe/src/index.ts
@@ -1,1 +1,5 @@
 import '../../assets/css/style.css';
+// import './swipe';
+import { swipe$ } from './swipe';
+
+swipe$.subscribe(console.log);
--- 1-swipe/src/swipe.ts
+++ 1-swipe/src/swipe.ts
@@ -1,1 +1,51 @@
-import '../../assets/css/style.css';
+import { filter, fromEvent, map, merge, Observable, pipe, zip } from "rxjs";
+
+// First
+// const mouseDown$ = fromEvent<MouseEvent>(document, 'mousedown');
+// const mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
+// const touchStart$ = fromEvent<MouseEvent>(document, 'touchstart');
+// const touchEnd$ = fromEvent<MouseEvent>(document, 'touchend');
+
+// const down$ = merge(mouseDown$, touchStart$);
+// const up$ = merge(mouseUp$, touchEnd$);
+
+// function getXPosition$(source$: Observable<MouseEvent | TouchEvent>): Observable<number> {
+//     return source$.pipe(
+//         map(event => event instanceof MouseEvent
+//             ? event.clientX
+//             : event.changedTouches.item(0)!.clientX,
+//         ),
+//     )
+// }
+
+// export const swipe$ = zip(getXPosition$(down$), getXPosition$(up$)).pipe(
+    // map(([start, end]) => start - end),
+    // filter(diff => !(diff < 20 && diff > -20)),
+// );
+
+// Alt
+function getXPosition$(source$: Observable<MouseEvent | TouchEvent>): Observable<number> {
+    return source$.pipe(
+        map(event => event instanceof MouseEvent
+            ? event.clientX
+            : event.changedTouches.item(0)!.clientX,
+        ),
+    )
+}
+
+const mouseDown$ = fromEvent<MouseEvent>(document, 'mousedown');
+const mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
+const zipXPositionMouseEvent$ = zip(getXPosition$(mouseDown$), getXPosition$(mouseUp$));
+
+const touchStart$ = fromEvent<MouseEvent>(document, 'touchstart');
+const touchEnd$ = fromEvent<MouseEvent>(document, 'touchend');
+const zipXPositionTouchEvent$ = zip(getXPosition$(touchStart$), getXPosition$(touchEnd$));
+
+const getDiffWithFilter = pipe(
+    map(([start, end]) => start - end),
+    filter(diff => !(diff < 20 && diff > -20)),
+)
+
+export const swipe$ = merge(zipXPositionMouseEvent$, zipXPositionTouchEvent$).pipe(
+    getDiffWithFilter
+)
