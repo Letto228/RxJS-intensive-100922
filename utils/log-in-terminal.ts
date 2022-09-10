26924d860f7adf0f33927ccb09e9d12154e1df39
e.g.sidorov <e.g.sidorov@tinkoff.ru>
2022-09-10 11:16:54 +0300

    init commit

--- utils/log-in-terminal.ts
+++ utils/log-in-terminal.ts
@@ -1,5 +1,5 @@
 const terminal = document.querySelector('.terminal pre') as HTMLPreElement;
 
-export function terminalLog(text: string, where: InsertPosition = 'beforeend') {
+export function terminalLog<T>(text: T, where: InsertPosition = 'beforeend') {
     terminal.insertAdjacentHTML(where, `<div>${text}</div>`)
 }
