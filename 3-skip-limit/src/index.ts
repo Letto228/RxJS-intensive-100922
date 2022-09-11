3d45da19d3d65d5fceb4ce2e712c6e705314ec1e
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-09-11 16:04:21 +0300

    skip limit task

--- 3-skip-limit/src/index.ts
+++ 3-skip-limit/src/index.ts
@@ -1,1 +1,5 @@
+import { interval, take } from 'rxjs';
 import '../../assets/css/style.css';
+import {skipLimit} from './skip-limit';
+
+interval(1000).pipe(take(10), skipLimit(2, 2)).subscribe(console.log);
---
+++ 3-skip-limit/src/skip-limit.ts
@@ -0,0 +1,40 @@
+// --- 1000ms
+// ---0---1---2---3---4---5---6---7---8---9|
+// skipLimit(2, 2)
+// ---_---_---2---3---_---_---6---7---_---_| 
+
+import { Observable } from "rxjs";
+
+export function skipLimit<T>(skip: number, limit: number): (source$: Observable<T>) => Observable<T> {
+    return (source$: Observable<T>): Observable<T> => {
+        let interval = 1;
+        let valueIndex = 1;
+
+        return new Observable<T>(subscriber => {
+            const subscription = source$.subscribe({
+                ...subscriber,
+                next: value => {
+                    const borderRight = interval * (skip + limit);
+                    const borderLeft = borderRight - limit;
+
+                    const isNeedValue = valueIndex > borderLeft && valueIndex <= borderRight;
+
+                    if (isNeedValue) {
+                        subscriber.next(value);
+
+                    }
+
+                    valueIndex += 1;
+
+                    if (borderRight < valueIndex) {
+                        interval += 1;
+                    }
+                }
+            });
+
+            return () => {
+                subscription.unsubscribe();
+            }
+        })
+    }
+}
